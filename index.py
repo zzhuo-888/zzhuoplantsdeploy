c56d7635cf1aa129c62845744fb16ea737d35145
zzhuo-888 <zzhuo_888@163.com>
2023-05-29 10:07:10 +0800

    may

--- index.py
+++ index.py
@@ -517,7 +517,7 @@ def main():
                             # print("%d--%d" % (cloumn[0], cloumn[1]))
                             datetimenow = datetime.datetime.now()
                             datetimenow = f'{datetimenow}'.split('.')[0]
-                            datetimenow=list(datetimenow)
+                            datetimenow=str(datetimenow)
                             for i in range(len(datetimenow)):
                                 flag=0
                                 if(flag==0 and datetimenow[i]==':'):
@@ -525,6 +525,8 @@ def main():
                                     flag=1
                                 elif(flag==1 and datetimenow[i]==':'):
                                     datetimenow[i]='分'
+                            miao='秒'
+                            datetimenow=datetimenow+miao
 
 
                             add_searchdata(username, datetimenow, dname1)
